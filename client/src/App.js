f461e432a72547cdee26e15af93a9e15419bf687
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2024-09-19 18:04:26 +0100

    interface made for main blog page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
 import './App.css';
 import axios from "axios";
-import {useEffect} from 'react';
+import {useEffect, useState } from 'react';
 
 function App() {
 
+  const [listOfPosts, setListOfPosts] = useState([]);
+
   useEffect(() => {
     axios.get("http://localhost:3001/Posts").then((response) => {
-      console.log(response);
+      setListOfPosts(response.data)
     })
   }, [])
 
   return (
     <div className="App">
-      
+      <div className="apptitle">
+        <h1>Blog Post App</h1>
+      </div>
+      <div className="wholeListOfPosts">
+      {listOfPosts.map((value, key) => { 
+        return (
+            <div className="post">
+              <div className="title"><h2>{value.title}</h2></div>
+              <div className="body">{value.postText}</div>
+              <div className="footer">Posted by user: {value.username}</div>
+            </div>)
+      })}
+      </div>
     </div>
   );
 }
